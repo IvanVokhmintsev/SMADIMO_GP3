4b4b04eed77660932ae6e21314c5c782e211648b
IvanVokhmintsev <vokhmintsev.vanya@gmail.com>
2026-04-27 14:06:12 +0300

    Пупка залупка

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ from langchain.agents import create_agent
 
 from dotenv import load_dotenv
 from PipelineMemory import PipelineMemory
+from tools.download_file import make_download_tools
 from tools.make_eda_tools import make_eda_tools
 
 load_dotenv()
 
 state = PipelineMemory()
 
+download_tools = make_download_tools(state)
 eda_tools = make_eda_tools(state)
 
-def get_weather(city: str) -> str:
-    """Get weather for a given city."""
-    return f"It's always sunny in {city}!"
 
 agent = create_agent(
     model="google_genai:gemini-2.5-flash-lite",
-    tools=[get_weather],
+    tools=download_tools,
     system_prompt="You are a helpful assistant",
 )
 
 result = agent.invoke(
-    {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
+    {"messages": [{"role": "user", "content": "Скачай файл по данному url https://drive.usercontent.google.com/u/0/uc?id=1PDpIeIhzFJnEtaSUZmtZPM2pq8FxvfhF&export=download"}]}
 )
 print(result["messages"][-1].content_blocks)
